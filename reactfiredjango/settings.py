76876516e06fb61ea2f284d784af24ff4d4722a5
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-03-04 13:38:32 +0300

    Add crispy forms and drf

--- reactfiredjango/settings.py
+++ reactfiredjango/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'crispy_forms',
+    'rest_framework',
+
     'todo'
 ]
 
@@ -120,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
