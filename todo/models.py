397b015c32ea9c401efa1905defcddb3dc012e52
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-03-04 13:40:23 +0300

    Add ToDo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    name = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
