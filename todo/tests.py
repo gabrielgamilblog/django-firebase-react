60c26008aafc022821e8d6d7709dba32e9cb3f70
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-03-04 15:41:06 +0300

    Add basic tests

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import ToDo
+
+
+class ToDoTestCase(TestCase):
+    def test_create_todo(self):
+        response = self.client.post(reverse('create-todo'), {'name': 'Test my todo'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(ToDo.objects.count(), 1)
+
+
+    def test_dash(self):
+        response = self.client.get(reverse('dash'))
+        self.assertEqual(response.status_code, 200)
