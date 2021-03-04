8a3065435fd14d8473294c3de915043da74a811d
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-03-04 15:40:54 +0300

    Add object detail url

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ToDo(models.Model):
     name = models.CharField(max_length=255)
     is_done = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('dash')
