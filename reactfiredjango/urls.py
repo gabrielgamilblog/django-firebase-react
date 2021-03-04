b7352ccdf6f73f13cb8af10ba977cf89f28f4b04
Gabriel Gamil <gamil.gabriel@gmail.com>
2021-03-04 11:01:50 +0300

    Hook the app and the page

--- reactfiredjango/urls.py
+++ reactfiredjango/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todo.views import DashView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', DashView.as_view(), name='dash')
 ]
